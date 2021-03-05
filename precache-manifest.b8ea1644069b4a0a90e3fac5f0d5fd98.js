c56579465563029881db41c23d08d0d3c8290712
toronir <toronit5@gmail.com>
2021-03-05 12:51:52 +0100

    Create precache-manifest.b8ea1644069b4a0a90e3fac5f0d5fd98.js

---
+++ precache-manifest.b8ea1644069b4a0a90e3fac5f0d5fd98.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b36d3235566104d97e9abe199bb7df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faa3583a112dd4f8787c",
+    "url": "/static/css/3.2e7c903d.chunk.css"
+  },
+  {
+    "revision": "5fa521b4813fbc620835",
+    "url": "/static/css/main.5a2d6e79.chunk.css"
+  },
+  {
+    "revision": "51e419125dd9eecc3705",
+    "url": "/static/js/2.bb81e799.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bb81e799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa3583a112dd4f8787c",
+    "url": "/static/js/3.35283644.chunk.js"
+  },
+  {
+    "revision": "5fa521b4813fbc620835",
+    "url": "/static/js/main.2c8c98ef.chunk.js"
+  },
+  {
+    "revision": "072ed9130bef4c36af57",
+    "url": "/static/js/runtime-main.3701e6c1.js"
+  },
+  {
+    "revision": "48b6bce4de713b195493abd0dbfdb5d3",
+    "url": "/static/media/d1.48b6bce4.gif"
+  }
+]);
